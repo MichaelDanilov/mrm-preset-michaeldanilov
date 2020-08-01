8fd976e6c2a5d30954dc8c32349b5b6fe0f86bd5
Michael Danilov <michael@danilov.me>
2020-08-01 16:09:33 +0300

    prettier by format

--- eslint/index.js
+++ eslint/index.js
@@ -1,68 +1,68 @@
-const { json, lines, packageJson, install } = require("mrm-core");
+const { json, lines, packageJson, install } = require('mrm-core');
 
 function task() {
   const packages = [
-    "@typescript-eslint/eslint-plugin",
-    "@typescript-eslint/parser",
-    "eslint",
-    "eslint-config-airbnb-base",
-    "eslint-config-prettier",
-    "eslint-plugin-import",
-    "eslint-plugin-prettier",
-    "npm-run-all",
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint/parser',
+    'eslint',
+    'eslint-config-airbnb-base',
+    'eslint-config-prettier',
+    'eslint-plugin-import',
+    'eslint-plugin-prettier',
+    'npm-run-all',
   ];
 
   // .eslintrc
-  const eslintrc = json(".eslintrc.json");
+  const eslintrc = json('.eslintrc.json');
 
-  eslintrc.set("extends", [
-    "airbnb-base",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
+  eslintrc.set('extends', [
+    'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
   ]);
-  eslintrc.set("parser", "@typescript-eslint/parser");
-  eslintrc.set("plugins", ["prettier", "@typescript-eslint"]);
-  eslintrc.set("globals", {});
-  eslintrc.set("rules", {
-    "prettier/prettier": "error",
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+  eslintrc.set('parser', '@typescript-eslint/parser');
+  eslintrc.set('plugins', ['prettier', '@typescript-eslint']);
+  eslintrc.set('globals', {});
+  eslintrc.set('rules', {
+    'prettier/prettier': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
-    "@typescript-eslint/no-var-requires": "off",
+    '@typescript-eslint/no-var-requires': 'off',
   });
-  eslintrc.set("settings", {
-    "import/resolver": {
+  eslintrc.set('settings', {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   });
 
   eslintrc.save();
 
-  lines(".eslintignore").set(["node_modules", "dist"]).save();
+  lines('.eslintignore').set(['node_modules', 'dist']).save();
 
   const pkg = packageJson();
 
   pkg
     // Add lint script
-    .setScript("lint:js", "eslint --ext .js,.jsx .")
-    .setScript("lint:ts", "eslint --ext .ts,.tsx .")
-    .appendScript("lint", "run-p lint:*")
+    .setScript('lint:js', 'eslint --ext .js,.jsx .')
+    .setScript('lint:ts', 'eslint --ext .ts,.tsx .')
+    .appendScript('lint', 'run-p lint:*')
     .save();
 
   // Dependencies
   install(packages);
 }
 
-task.description = "Adds ESLint";
+task.description = 'Adds ESLint';
 module.exports = task;
--- nvm/index.js
+++ nvm/index.js
@@ -1,8 +1,8 @@
-const { lines } = require("mrm-core");
+const { lines } = require('mrm-core');
 
 function task() {
-  lines(".nvmrc").set(["12"]).save();
+  lines('.nvmrc').set(['12']).save();
 }
 
-task.description = "Adds Nvmrc";
+task.description = 'Adds Nvmrc';
 module.exports = task;
