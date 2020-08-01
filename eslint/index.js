3626501c617711897a90d39a309e630062bb955f
Michael Danilov <michael@danilov.me>
2020-08-01 14:01:26 +0300

    update eslint preset

--- eslint/index.js
+++ eslint/index.js
@@ -1,38 +1,68 @@
-const { json, packageJson, install } = require('mrm-core');
+const { json, lines, packageJson, install } = require("mrm-core");
 
 function task() {
   const packages = [
-    'eslint',
-    'eslint-config-airbnb-base',
-    'eslint-import-resolver-node',
-    'eslint-plugin-import',
+    "@typescript-eslint/eslint-plugin",
+    "@typescript-eslint/parser",
+    "eslint",
+    "eslint-config-airbnb-base",
+    "eslint-config-prettier",
+    "eslint-plugin-import",
+    "eslint-plugin-prettier",
+    "npm-run-all",
   ];
 
   // .eslintrc
-  const eslintrc = json('.eslintrc');
-  eslintrc.set('extends', 'airbnb-base');
-  eslintrc.set('globals', {});
-  eslintrc.set('settings', {
-    'import/resolver': {
+  const eslintrc = json(".eslintrc.json");
+
+  eslintrc.set("extends", [
+    "airbnb-base",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ]);
+  eslintrc.set("parser", "@typescript-eslint/parser");
+  eslintrc.set("plugins", ["prettier", "@typescript-eslint"]);
+  eslintrc.set("globals", {});
+  eslintrc.set("rules", {
+    "prettier/prettier": "error",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "@typescript-eslint/no-var-requires": "off",
+  });
+  eslintrc.set("settings", {
+    "import/resolver": {
       node: {
-        paths: ['./'],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
     },
   });
 
   eslintrc.save();
 
+  lines(".eslintignore").set(["node_modules", "dist"]).save();
+
   const pkg = packageJson();
 
   pkg
     // Add lint script
-    .setScript('lint:js', './node_modules/.bin/eslint --ext .js app')
-    .appendScript('lint', 'npm run lint:js')
+    .setScript("lint:js", "eslint --ext .js,.jsx .")
+    .setScript("lint:ts", "eslint --ext .ts,.tsx .")
+    .appendScript("lint", "run-p lint:*")
     .save();
 
   // Dependencies
   install(packages);
 }
 
-task.description = 'Adds ESLint';
+task.description = "Adds ESLint";
 module.exports = task;
